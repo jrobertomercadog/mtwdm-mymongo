83a21395f2a51a9c532181a0a3366a7d3472124d
jrobertomercadog <58094733+jrobertomercadog@users.noreply.github.com>
2020-05-02 03:18:59 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-FILE=/data/db/ready
-if [[ -f "$FILE" ]]; then
-    exit 0
-fi
 
-sleep 11
-cd /tmp/mymongo/mtwdm-mymongo 
-mongo admin adminstuff.js 
-mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 dboperator.js 
-mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 mydata.js
-touch /data/db/ready
+trap "echo TRAPed signal" HUP INT QUIT TERM
+
+mongod --auth &
+
+FILE=/data/db/ready
+if [[ ! -f "$FILE" ]]; then
+    sleep 11
+    cd /tmp/mymongo/mtwdm-mymongo 
+    mongo admin adminstuff.js 
+    mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 dboperator.js 
+    mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 mydata.js
+    touch /data/db/ready
+fi
+echo "[hit enter key to exit] or run docker stop <container>"
+read
 exit 0
