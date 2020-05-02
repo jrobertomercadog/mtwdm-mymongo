5b87ebc5cc5459bf94b60694acb08d4c6d9220ac
jrobertomercadog <58094733+jrobertomercadog@users.noreply.github.com>
2020-05-02 03:02:30 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-mongod --noauth &> /dev/null &
+FILE=/data/db/ready
+if [[ -f "$FILE" ]]; then
+    exit 0
+fi
+
 sleep 11
 cd /tmp/mymongo/mtwdm-mymongo 
 mongo admin adminstuff.js 
-mongo mtwdm dboperator.js 
-mongo mtwdm mydata.js
-kill $(pidof mongod)
+mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 dboperator.js 
+mongo --username=dba-root --password=whateverman --authenticationDatabase admin --host 127.0.0.1 --port 27017 mydata.js
+touch /data/db/ready
 exit 0
